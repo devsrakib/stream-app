579974b4206398dc499ad3a829142c8824d28577
devsrakib <devs.rakibulislam@gmail.com>
2024-12-11 13:19:31 +0600

    updated stream

--- app/(call)/_layout.tsx
+++ app/(call)/_layout.tsx
@@ -4,16 +4,74 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { Redirect, Tabs } from 'expo-router'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Colors, TABBAR_MAIN_COLOR } from '@/constants/Colors'
-import { useAuth } from '@clerk/clerk-expo'
+import { useAuth, useUser } from '@clerk/clerk-expo'
+import {
+    StreamCall,
+    StreamVideo,
+    StreamVideoClient,
+    User,
+    Logger
+  } from "@stream-io/video-react-native-sdk";
+
+
+const apiKey = process.env.EXPO_PUBLIC_GET_STEAM_API_KEY
+
+if(!apiKey){
+    throw new Error('Missing api key')
+}
 
 const CallRoutesLayout = () => {
     const { isSignedIn } = useAuth()
+    const {user:clerkUser} = useUser();
 
-    if (!isSignedIn) {
+
+    if (!isSignedIn || !clerkUser || !apiKey) {
         return <Redirect href={'/(auth)/sign-in'} />
     }
+
+    const tokenProvider = async () =>{
+const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/generateUserToken`, {
+    method: 'POST',
+    headers:{
+        'Content-type':'application/json'
+    },
+    body:JSON.stringify({
+        userId: clerkUser?.id,
+        name: clerkUser?.fullName,
+        image: clerkUser?.imageUrl,
+        email: clerkUser?.primaryEmailAddress?.toString()
+    })
+})
+const data = await response.json();
+return data?.token
+    }
+
+    const user:User = {
+        id: clerkUser?.id,
+        name: clerkUser?.firstName!,
+        image: clerkUser?.imageUrl
+
+    }
+
+    const myLogger: Logger = (logLevel, message, ...args) => {
+       
+      };
+    const client = StreamVideoClient.getOrCreateInstance({
+        apiKey,
+        tokenProvider,
+        user,
+        options: {
+          logLevel: "info",
+          logger: myLogger,
+        },
+      });
+
+    
     return (
         <SafeAreaView style={styles.container}>
+            <StreamVideo client={client}>
+
+           
             <Tabs screenOptions={({ route }) => ({
                 header: () => null,
                 tabBarStyle: {
@@ -89,6 +147,7 @@ const CallRoutesLayout = () => {
                     }}
                 />
             </Tabs>
+            </StreamVideo>
         </SafeAreaView>
     )
 }
